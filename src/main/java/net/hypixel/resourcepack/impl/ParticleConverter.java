cca11a75d78a53a53f17734513b3d6b277dfe060
CoryBorek <agentdid127cb@aol.com>
2019-12-16 10:07:22 -0500

    in-progress chest conversion

--- src/main/java/net/hypixel/resourcepack/impl/ParticleConverter.java
+++ src/main/java/net/hypixel/resourcepack/impl/ParticleConverter.java
@@ -1,6 +1,7 @@
 package net.hypixel.resourcepack.impl;
 
 import net.hypixel.resourcepack.Converter;
+import net.hypixel.resourcepack.Options;
 import net.hypixel.resourcepack.PackConverter;
 import net.hypixel.resourcepack.extra.ImageConverter;
 import net.hypixel.resourcepack.pack.Pack;
@@ -19,12 +20,9 @@ public class ParticleConverter extends Converter {
         Path imagePath = pack.getWorkingPath().resolve("assets" + File.separator + "minecraft" + File.separator + "textures" + File.separator + "particle" + File.separator + "particles.png");
         if (!imagePath.toFile().exists()) return;
 
+        //Particles
         int defaultW = 128, defaultH = 128;
         ImageConverter iconvert = new ImageConverter(defaultW, defaultH, imagePath);
-
-        // TODO check how higher resolution will handle this.
-
-            // make a new bigger image and just paste the existing on in the top left corner
             iconvert.newImage(iconvert.getWidth()*2, iconvert.getHeight()*2);
             iconvert.subImage(0,0,128,128,0,0);
             iconvert.store();
