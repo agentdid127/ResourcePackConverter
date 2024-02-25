a74cc36b5b4b00c2ef4435d4f960d38c06ceb501
enqorman <jejojora2@gmail.com>
2024-02-25 17:18:07 -0500

    Simplify more logic

--- library/src/main/java/com/agentdid127/resourcepack/library/utilities/ImageConverter.java
+++ library/src/main/java/com/agentdid127/resourcepack/library/utilities/ImageConverter.java
@@ -87,12 +87,10 @@ public class ImageConverter {
         int wMultiplier = getWidthMultiplier();
         int hMultiplier = getHeightMultiplier();
 
-        int x3;
-        int y3;
         int width2 = x2 * wMultiplier - x * wMultiplier;
         int height2 = y2 * hMultiplier - y * hMultiplier;
-        x3 = x == 0 ? 0 : x * wMultiplier;
-        y3 = y == 0 ? 0 : y * hMultiplier;
+        int x3 = x == 0 ? 0 : x * wMultiplier;
+        int y3 = y == 0 ? 0 : y * hMultiplier;
 
         BufferedImage part = getSubImage(x3, y3, width2, height2);
         g2d.drawImage(part, storex * wMultiplier, storey * hMultiplier, null);
@@ -107,12 +105,10 @@ public class ImageConverter {
         int wMultiplier = getWidthMultiplier();
         int hMultiplier = getHeightMultiplier();
         
-        int x3;
-        int y3;
         int width2 = x2 * wMultiplier - x * wMultiplier;
         int height2 = y2 * hMultiplier - y * hMultiplier;
-        x3 = x == 0 ? 0 : x * wMultiplier;
-        y3 = y == 0 ? 0 : y * hMultiplier;
+        int x3 = x == 0 ? 0 : x * wMultiplier;
+        int y3 = y == 0 ? 0 : y * hMultiplier;
 
         BufferedImage part = getSubImage(x3, y3, width2, height2);
         g2d.drawImage(createFlipped(part, flip), storex * wMultiplier, storey * hMultiplier, null);
