25eebf99fa789e0dae270c48d6ff7b68887a9050
lowercasebtw <ignzoudy@gmail.com>
2025-12-25 20:52:55 -0500

    Fix >=1.21.11 pack format error

--- conversions/Forwards/src/main/java/com/agentdid127/resourcepack/forwards/impl/other/PackMetaConverter.java
+++ conversions/Forwards/src/main/java/com/agentdid127/resourcepack/forwards/impl/other/PackMetaConverter.java
@@ -93,7 +93,7 @@ public class PackMetaConverter extends Converter {
 				packObject.addProperty("pack_format", packVersion);
 				if (packObject.has("supported_formats")) {
 					JsonObject supportedFormats = packObject.remove("supported_formats").getAsJsonObject();
-					packObject.addProperty("min_format", supportedFormats.get("min_inclusive").getAsDouble());
+					packObject.addProperty("min_format", Math.max(supportedFormats.get("min_inclusive").getAsDouble(), 65.0));
 					packObject.addProperty("max_format", supportedFormats.get("max_inclusive").getAsDouble());
 				}
 
